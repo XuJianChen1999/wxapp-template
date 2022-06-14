1a326f702f7595a4e3d743e1e1e2e0335cf664f5
徐建辰 <18253997748@163.com>
2022-06-14 16:31:05 +0800

    封装空数据组件

---
+++ components/Empty/index.js
@@ -0,0 +1,23 @@
+// components/Empty/index.js
+Component({
+  properties: {
+    src: {
+      type: String,
+      value: './empty.png'
+    },
+    text: {
+      type: String,
+      value: '暂无数据'
+    },
+    color: {
+      type: String,
+      value: '#969799'
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
